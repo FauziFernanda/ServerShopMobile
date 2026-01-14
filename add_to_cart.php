960cbbdbfeadabad0bc877dc5456ebc64b5bb5e7
Fauzi Fernanda <fauzifernanda1407@gmail.com>
2026-01-14 21:37:59 +0700

    update add_to_cart6

--- add_to_cart.php
+++ add_to_cart.php
@@ -8,17 +8,14 @@ $user_id    = $_POST['user_id'];
 $product_id = $_POST['product_id']; 
 $quantity   = isset($_POST['quantity']) ? $_POST['quantity'] : 1; 
 
-// Cek apakah barang sudah ada
 $checkCart = "SELECT * FROM cart WHERE user_id = '$user_id' AND product_id = '$product_id'";
 $result = $conn->query($checkCart);
 
 if ($result->num_rows > 0) {
-    // Update jumlah jika sudah ada
     $sql = "UPDATE cart SET quantity = quantity + $quantity 
             WHERE user_id = '$user_id' AND product_id = '$product_id'";
     $msg = "Jumlah barang berhasil ditambah";
 } else {
-    // Insert baru jika belum ada
     $sql = "INSERT INTO cart (user_id, product_id, quantity) 
             VALUES ('$user_id', '$product_id', '$quantity')";
     $msg = "Berhasil masuk keranjang";
