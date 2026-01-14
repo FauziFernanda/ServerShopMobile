f53352eb6854a5877134addb037ad861ed0f21cd
Fauzi Fernanda <fauzifernanda1407@gmail.com>
2026-01-14 17:59:10 +0700

    update cart

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,20 +1,21 @@
 <?php
 include_once 'dbconenct.php';
 
+$user_id = $_POST['user_id'];
 $product_id = $_POST['product_id'];
 
-$check = $conn->prepare("SELECT id FROM cart WHERE product_id = ?");
-$check->bind_param("i", $product_id);
+$check = $conn->prepare("SELECT id FROM cart WHERE product_id = ? AND user_id = ?");
+$check->bind_param("ii", $product_id, $user_id);
 $check->execute();
 $result = $check->get_result();
 
 if ($result->num_rows > 0) {
-    $stat = $conn->prepare("UPDATE cart SET quantity = quantity + 1 WHERE product_id = ?");
+    $stat = $conn->prepare("UPDATE cart SET quantity = quantity + 1 WHERE product_id = ? AND user_id = ?");
 } else {
-    $stat = $conn->prepare("INSERT INTO cart (product_id, quantity) VALUES (?, 1)");
+    $stat = $conn->prepare("INSERT INTO cart (user_id, product_id, quantity) VALUES (?, ?, 1)");
 }
 
-$stat->bind_param("i", $product_id);
+$stat->bind_param("ii", $product_id, $user_id);
 if ($stat->execute()) {
     echo json_encode(["status" => "success"]);
 } else {
