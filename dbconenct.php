d2e5c967cb0927170c2477da6698bfdbc85abd8a
Fauzi Fernanda <fauzifernanda1407@gmail.com>
2026-01-14 20:13:30 +0700

    update dbconnect

--- dbconenct.php
+++ dbconenct.php
@@ -1,7 +1,13 @@
 <?php
-define('DB_HOST', 'gondola.proxy.rlwy.net');
-define('DB_USER', 'root');
-define('DB_PASS', 'gFuuHcmQTuphAYilbpqJmCRQQpXokART');
-define('DB_NAME', 'railway');
-define('DB_PORT', 34844);
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
