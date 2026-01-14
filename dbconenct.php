7e50586202b0494d7500c89c9ac89cd934dbecaa
Fauzi Fernanda <fauzifernanda1407@gmail.com>
2026-01-14 18:29:37 +0700

    update dbconnect

--- dbconenct.php
+++ dbconenct.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', 'interchange.proxy.rlwy.net');
+define('DB_HOST', 'gondola.proxy.rlwy.net');
 define('DB_USER', 'root');
-define('DB_PASS', 'ZWCAomhdzcNBxWRIEgGMTPYiSKhngTgj');
+define('DB_PASS', 'gFuuHcmQTuphAYilbpqJmCRQQpXokART');
 define('DB_NAME', 'railway');
-define('DB_PORT', 58975);
+define('DB_PORT', 34844);
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
