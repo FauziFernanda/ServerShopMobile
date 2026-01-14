090e7f1a361e606eaef6c992d5885534c0626f7a
Fauzi Fernanda <fauzifernanda1407@gmail.com>
2026-01-14 20:45:29 +0700

    update add_to_cart3

--- add_to_cart.php
+++ add_to_cart.php
@@ -5,7 +5,7 @@ $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
 $product_id = isset($_POST['product_id']) ? $_POST['product_id'] : null;
 
 if (!$user_id || !$product_id) {
-    echo json_encode(["status" => "error", "message" => "Data tidak lengkap"]);
+    echo json_encode(["status" => "error", "message" => "Data user_id atau product_id kosong"]);
     exit;
 }
 
@@ -15,11 +15,9 @@ $check->execute();
 $result = $check->get_result();
 
 if ($result->num_rows > 0) {
-    
     $stat = $conn->prepare("UPDATE cart SET quantity = quantity + 1 WHERE product_id = ? AND user_id = ?");
     $stat->bind_param("ii", $product_id, $user_id);
 } else {
-    
     $stat = $conn->prepare("INSERT INTO cart (user_id, product_id, quantity, is_selected) VALUES (?, ?, 1, 0)");
     $stat->bind_param("ii", $user_id, $product_id);
 }
